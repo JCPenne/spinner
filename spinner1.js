9526212258e9ce403882c64c3062161cbbdfbc55
JCPenne <jackcpalmer@icloud.com>
2022-06-12 10:58:57 -0700

    final refactor to spinner1.js

--- spinner1.js
+++ spinner1.js
@@ -1,7 +1,7 @@
 let time = 100;
 const interval = 200;
 const length = 10;
-for (i = 0; i < length; i++) {
+for (let i = 0; i < length; i++) {
   setTimeout(() => {
     process.stdout.write('\r|   ');
   }, time);
